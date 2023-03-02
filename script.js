f30817ec8e974f5dde3f75a7300a75493076744f
Gevork Seropyan <seropyangevork@rocketmail.com>
2023-03-02 08:56:16 -0500

    added getItems from localStorage

--- script.js
+++ script.js
@@ -13,6 +13,17 @@ $(function () {
     //sets timekey and textInput defined above to localStorage
     localStorage.setItem(timeKey, textInput)
   })
+  // localStorage loads set items using getItem
+  $("#hour10 .description").val(localStorage.getItem("hour10"));
+  $("#hour11 .description").val(localStorage.getItem("hour11"));
+  $("#hour12 .description").val(localStorage.getItem("hour12"));
+  $("#hour13 .description").val(localStorage.getItem("hour13"));
+  $("#hour14 .description").val(localStorage.getItem("hour14"));
+  $("#hour15 .description").val(localStorage.getItem("hour15"));
+  $("#hour16 .description").val(localStorage.getItem("hour16"));
+  $("#hour17 .description").val(localStorage.getItem("hour17"));
 
 
+
+  
 });
